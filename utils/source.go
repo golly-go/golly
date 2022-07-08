5e34d91143dbebba1e7d12122f907f03c810afe2
Mitch Rodrigues <mitch@telmate.com>
2022-07-07 23:52:47 -0500

    Fix the logger to remove gorm

--- utils/source.go
+++ utils/source.go
@@ -20,7 +20,7 @@ func FileWithLineNum() string {
 	// the second caller usually from gorm internal, so set i start from 2
 	for i := 2; i < 15; i++ {
 		_, file, line, ok := runtime.Caller(i)
-		if ok && (!strings.HasPrefix(file, gollySourceDir) || strings.HasSuffix(file, "_test.go")) {
+		if ok && ((!strings.HasPrefix(file, gollySourceDir) && !strings.Contains(file, "gorm.io")) || strings.HasSuffix(file, "_test.go")) {
 			return file + ":" + strconv.FormatInt(int64(line), 10)
 		}
 	}
